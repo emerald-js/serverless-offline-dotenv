189c4f27e969c5dd5b8ababe49379fbe6bf4aaf5
MicMonen <michel@monen.com>
2018-08-20 15:57:27 +0200

    cleanup package

--- build.sh
+++ build.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env bash
 cd "$(dirname $0)";
 
-yellow='\e[33m'
-green='\e[32m'
-clear='\e[0m'
+yellow='\033[33m'
+green='\033[32m'
+clear='\033[m'
 
-for input in index.coffee
+for input in src{/**,}/*.coffee
 do
-    output="$(sed 's/\.coffee$/\.js/' <<< "${input}")"
+    output="$(sed 's/\.coffee$/\.js/' <<< "${input:4}")"
 
     echo -e "${yellow}>${clear} ${green}${input}${clear} to ${green}${output}${clear}";
     coffee -to "${output}" "${input}"
